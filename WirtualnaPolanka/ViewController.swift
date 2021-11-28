3e309eeaa82e01b14415ff987fbe134bc0446f17
Krzysztof Peda <krzysieq@Krzysztofs-iMac.local>
2021-11-28 19:01:12 +0100

    Initial Commit

---
+++ WirtualnaPolanka/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  WirtualnaPolanka
+//
+//  Created by Krzysztof Peda on 28/11/2021.
+//  Copyright © 2021 Krzysztof Peda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
